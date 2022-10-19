e7f01683fe2301ea9f41ff005695b941d821ffa6
husted123 <anders.husted@gmail.com>
2022-10-19 16:30:10 +0200

    signup redirect

--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -1,39 +1,49 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 import { apiModel } from '../../models/apiModel';
 
 export const Signup = () => {
-    const  [username, setUsername] = useState("");
-    const  [password, setPassword] = useState("");
-    const  [firstname, setFirstname] = useState("");
-    const  [lastname, setLastname] = useState("");
-    const  [height, setHeight] = useState(0);
-    const  [gender, setGender] = useState<"male" | "female">("male");
-    const  [birthday, setBirthday] = useState("");
+    const navigate = useNavigate();
 
- 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [height, setHeight] = useState(0);
+    const [gender, setGender] = useState<"male" | "female">("male");
+    const [birthday, setBirthday] = useState("");
+
+
 
     const selectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const value = e.target.value;
-        if (value === "male" || value === "female"){
+        if (value === "male" || value === "female") {
             setGender(value);
         }
     };
 
-    function submitHandler(e: React.FormEvent){
+    function submitHandler(e: React.FormEvent) {
         e.preventDefault();
-        const res = apiModel.signup(username, password, firstname, lastname, height, gender, birthday);
+        const res = apiModel.signup(username, password, firstname, lastname, height, gender, birthday)
+            .then((res) => {
+                if (res.succes === true) {
+                    navigate('/login')
+                }
+            })
+
+
     }
 
     return (
         <>
             <div>Signup</div>
             <form onSubmit={e => submitHandler(e)}>
-                <input type="text" placeholder='Username' onChange={e => setUsername(e.target.value)}/> <br />
-                <input type="password" placeholder='Password' onChange={e => setPassword(e.target.value)}/> <br />
+                <input type="text" placeholder='Username' onChange={e => setUsername(e.target.value)} /> <br />
+                <input type="password" placeholder='Password' onChange={e => setPassword(e.target.value)} /> <br />
                 <br />
-                <input type="text" placeholder='First Name' onChange={e => setFirstname(e.target.value)}/> <br />
-                <input type="text" placeholder='Last Name' onChange={e => setLastname(e.target.value)}/> <br />
-                <input type="number" min="1" max="999" placeholder='Height'onChange={e => setHeight(parseInt(e.target.value))}/> <br />
+                <input type="text" placeholder='First Name' onChange={e => setFirstname(e.target.value)} /> <br />
+                <input type="text" placeholder='Last Name' onChange={e => setLastname(e.target.value)} /> <br />
+                <input type="number" min="1" max="999" placeholder='Height' onChange={e => setHeight(parseInt(e.target.value))} /> <br />
                 <select onChange={selectChange}>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
