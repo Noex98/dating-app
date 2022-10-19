657c832ad93ea3a25c870d427d58a4b51206c674
husted123 <anders.husted@gmail.com>
2022-10-19 10:00:31 +0200

    signup html

--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export const Signup = () => {
     return (
-        <div>Signup</div>
+        <>
+            <div>Signup</div>
+            <form action="">
+                <input type="text" name="name" placeholder='Username' /> <br />
+                <input type="password" name="name" placeholder='Password' /> <br />
+                <br />
+                <input type="text" name="name" placeholder='First Name' /> <br />
+                <input type="text" name="name" placeholder='Last Name' /> <br />
+                <input type="text" name="name" placeholder='Height' /> <br />
+                <input type="text" name="name" placeholder='Birthday' /> <br />
+                <select name="" id="">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select><br />
+                <input type="date" id="start" name="trip-start"
+                    value="0000-00-00"
+                    min="2004-01-01" max="3000-12-31">
+                </input>
+            </form>
+        </>
     )
 }
