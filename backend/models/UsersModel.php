ed75fb3615441081c02ee0704366d86e630d74c2
DanielTjerrildGamborg <dtg@heymate.dk>
2022-10-19 15:39:58 +0200

    191022

--- backend/models/UsersModel.php
+++ backend/models/UsersModel.php
@@ -29,6 +29,26 @@ class UserModel
         return $users;
     }
 
+    function getMatches($ammount)
+    {
+        $q = "SELECT * FROM userprofile ORDER BY id DESC LIMIT $ammount";
+        $res = $this->mySQL->query($q);
+        $users = [];
+
+        while ($row = $res->fetch_object()) {
+            array_push($users, [
+                "id" => $row->id,
+                "firstname" => $row->firstname,
+                "lastname" => $row->lastname,
+                "email" => $row->email,
+                "username" => $row->username
+            ]);
+        }
+
+        return $users;
+    }
+
+
     function getCurrentUser()
     {
         if (!isset($_SESSION)) {
