9ddf6bf2dc4c0005935379300b044783ef69dbc3
husted123 <anders.husted@gmail.com>
2022-10-21 12:51:31 +0200

    .

--- backend/models/UsersModel.php
+++ backend/models/UsersModel.php
@@ -34,7 +34,7 @@ class UserModel
         $q = "SELECT * FROM preferences WHERE id = '$id'";
         $result = $this->mySQL->query($q);
         $preferences = mysqli_fetch_assoc($result);    
-        $q ="CALL getPotentialMatches(" . $preferences['heightMin'] .  $preferences['heightMax'] . $preferences['ageMin'] . $preferences['ageMax'] . $preferences['gender'] . ")"; 
+        $q ="CALL getPotentialMatches('" . $preferences['heightMin']."', '" .  $preferences['heightMax']."', '" . $preferences['ageMin']."', '" . $preferences['ageMax']."', '" . $preferences['gender']. "' ')"; 
         $result = $this->mySQL->query($q);
         return $result->fetch_object();
     }
